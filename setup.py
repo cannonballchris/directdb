cceec4c93747de4d543f997c68da711a14d006c9
CannonBall Chris <cannonballchris8@gmail.com>
2024-05-27 21:32:18 +0530

    update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '1.1.5'
+VERSION = '1.1.6'
 DESCRIPTION = 'An async package that makes database handling extremely easy!'
 
 # Setting up
